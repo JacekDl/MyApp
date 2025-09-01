b6521845c566101e09d50d34adb70b9f37751e3f
Jacek <ja.d@wp.pl>
2025-09-01 17:06:33 +0200

    Created User model.

---
+++ src/MyApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required, EmailAddress, MaxLength(256)]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+}
