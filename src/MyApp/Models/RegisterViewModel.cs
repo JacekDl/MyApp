7911c887a214a5b70afa43c990fd0f96028e00b8
Jacek <ja.d@wp.pl>
2025-09-01 17:21:01 +0200

    Created RegisterViewModel.

---
+++ src/MyApp/Models/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyApp.Models;
+
+public class RegisterViewModel
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password), MinLength(8)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password), Compare(nameof(Password))]
+    [Display(Name = "Confirm Password")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
