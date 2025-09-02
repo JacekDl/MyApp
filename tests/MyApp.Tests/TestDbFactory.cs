84c51ff1275840005a24f60d7c0c86b7577d965f
Jacek <ja.d@wp.pl>
2025-09-02 16:55:03 +0200

    Implemented TestDbFactory class.

--- tests/MyApp.Tests/TestDbFactory.cs
+++ tests/MyApp.Tests/TestDbFactory.cs
@@ -1,10 +1,25 @@
-﻿namespace MyApp.Tests;
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using MyApp.Data;
 
-public class TestDbFactory
+namespace MyApp.Tests;
+
+public static class TestDbFactory
 {
-    [Fact]
-    public void Test1()
+    public static (ApplicationDbContext Db, SqliteConnection Connection) CreateSqliteInMemoryDb()
     {
+        var conn = new SqliteConnection("Filename=:memory:");
+        conn.Open();
 
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseSqlite(conn)
+            .EnableSensitiveDataLogging()
+            .Options;
+
+        var db = new ApplicationDbContext(options);
+        db.Database.EnsureCreated();
+
+        return (db, conn);
     }
 }
