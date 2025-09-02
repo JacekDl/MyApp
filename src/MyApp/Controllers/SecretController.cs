a0fc346b2fb4ce6a7b1a22d0561cbb148538b0a6
Jacek <ja.d@wp.pl>
2025-09-02 14:46:03 +0200

    Modified SecretController - added [Authorize] attribute.

--- src/MyApp/Controllers/SecretController.cs
+++ src/MyApp/Controllers/SecretController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyApp.Controllers;
 
+[Authorize]
 public class SecretController : Controller
 {
     public IActionResult Index()
