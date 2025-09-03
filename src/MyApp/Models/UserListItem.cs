c53ca0da7288c99c96602ef8a9b54671deaec55f
Jacek <ja.d@wp.pl>
2025-09-03 10:38:46 +0200

    Created UserListItem DTO.

---
+++ src/MyApp/Models/UserListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.Models;
+
+public sealed class UserListItem
+{
+    public int Id { get; init; }
+    public string Email { get; init; } = string.Empty;
+    public string Role { get; init; } = "User";
+    public DateTime CreatedAt { get; init; }
+}
