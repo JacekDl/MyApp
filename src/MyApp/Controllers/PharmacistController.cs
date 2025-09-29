0bab7a6d863462d4831a1917c01ec0ce47aab038
Jacek <ja.d@wp.pl>
2025-09-29 19:48:08 +0200

    Moved some actions from PharmacistController to PatientController.

--- src/MyApp/Controllers/PharmacistController.cs
+++ src/MyApp/Controllers/PharmacistController.cs
@@ -44,57 +44,6 @@ public class PharmacistController(IReviewPdfService pdfService, IMediator mediat
     }
     #endregion
 
-    #region PublicEditReview
-
-    [AllowAnonymous, HttpGet("/r/{number}")]
-    public async Task<IActionResult> PublicEdit(string number)
-    {
-        var result = await mediator.Send(new GetReviewQuery(number));
-
-        if (!result.IsSuccess)
-        {
-            return NotFound(); //View with error message can be implemented here.
-        }
-
-        var review = result.Value!;
-        var vm = new PublicReviewEditViewModel
-        {
-            Advice = review.Text,
-            Number = review.Number,
-            ReviewText = review.ReviewText ?? string.Empty
-        };
-        return View(vm);
-    }
-
-
-    [AllowAnonymous, HttpPost("/r/{number}"), ValidateAntiForgeryToken]
-    public async Task<IActionResult> PublicEdit(string number, PublicReviewEditViewModel vm)
-    {
-        if (number != vm.Number)
-        {
-            return BadRequest();
-        }
-
-        var result = await mediator.Send(new UpdateReviewCommand(number, vm.ReviewText));
-
-        if (!result.IsSuccess)
-        {
-            return NotFound(); //TODO: View with error message can be implemented here.
-        }
-
-        TempData["Saved"] = true;
-        return RedirectToAction("CompleteEdit", new {number});
-    }
-
-    [AllowAnonymous, HttpGet("/r/complete")]
-    public IActionResult CompleteEdit()
-    {
-        return View();
-    }
-
-
-    #endregion
-
     #region ListUsersReviews
     [HttpGet]
     public async Task<IActionResult> Tokens(string? searchTxt, bool? completed)
