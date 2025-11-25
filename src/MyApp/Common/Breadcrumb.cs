e261249e0e9a1448a8bfd218637e0ea7c746f4b5
Jacek <ja.d@wp.pl>
2025-11-25 08:24:51 +0100

    Dodano klasę Breadcrumb.cs i BaseViewModel.cs jej używający.

---
+++ src/MyApp/Common/Breadcrumb.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.Web.Common
+{
+    public class Breadcrumb
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Action { get; set; } = string.Empty;
+        public string Controller { get; set; } = string.Empty;
+    }
+}
---
+++ src/MyApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MyApp.Web.Common;
+
+namespace MyApp.Web.ViewModels
+{
+    public class BaseViewModel
+    {
+        public List<Breadcrumb> Breadcrumbs { get; set; } = [];
+    }
+}
