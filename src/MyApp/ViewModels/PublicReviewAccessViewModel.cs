68709b872546a5e9b1b649231d8d43c36ed20870
Jacek <ja.d@wp.pl>
2026-01-20 08:52:04 +0100

    Changed buttons and did some cleaning.

--- src/MyApp/ViewModels/PublicReviewAccessViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyApp.Web.ViewModels;
-
-public class PublicReviewAccessViewModel : ViewModelBase
-{
-    public string Number { get; set; } = string.Empty;
-    public string Advice { get; set; } = string.Empty;
-}
