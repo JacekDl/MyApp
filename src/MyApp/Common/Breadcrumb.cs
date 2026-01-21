f2c245f09db8786c7e843b8341d0e45258099ddb
Jacek <jacek.dlugopolski@vulcan.edu.pl>
2026-01-21 10:45:50 +0100

    Changed Index page.

--- src/MyApp/Common/Breadcrumb.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyApp.Web.Common
-{
-    public class Breadcrumb
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Action { get; set; } = string.Empty;
-        public string Controller { get; set; } = string.Empty;
-    }
-}
